2a56bef023cfde64e8c4a6556fb2521333e66ece
shupergirl <35987330+shupergirl@users.noreply.github.com>
2018-01-31 11:46:17 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "formget#123"){
+if ( username == "shruti" && password == "shuper@123"){
 alert ("Login successfully");
 // window.location = "success.html"; // Redirecting to other page.
 // return false;
